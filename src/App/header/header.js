7f65d161da3eb735c67072a3694481b5fcfbe719
Anton <antonbendrik1993@gmail.com>
2020-06-03 20:24:03 +0300

    added background-images to header

--- src/App/header/header.js
+++ src/App/header/header.js
@@ -3,7 +3,20 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            <header></header>
+            <header>
+                <div className="Group154Left">
+                    <div className='f3'></div>
+                    <div className='f1_2'></div>
+                    <div className='f1_2_1'></div>
+                    <div className='f2_2'></div>
+                </div>
+                <div className='inline-header'>
+                    <div className="leftGirl"></div>
+                    <div className="header-content-container"></div>
+                    <div className="rightGirl"></div>
+                </div>
+                <div className="Group154Right"></div>
+            </header>
         </div>
     )
 }
